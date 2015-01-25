df1f35bfb3f580469ce5d713a020551fe9fdb46c
Nico Grashoff <ngr@abctec.de>
2015-01-25 16:49:58 +0100

    Polish win screen

--- states/win.lua
+++ states/win.lua
@@ -1,14 +1,28 @@
-win = {}
+win = {
+	quotes = {
+		{ quote = "Act the way you’d like to be and soon you’ll be the way you’d like to act.", author = "Bob Dylan" }
+	},
+	text = {
+		"You made it! Be proud of yourself!",
+		"",
+		"",
+		"Press ESC to quit",
+		"",
+		"Press any key to return to the menu" 
+	}
+}
 
 function win:enter()
 	love.graphics.setFont(fonts.text)
 end
 
 function win:draw()
-	love.graphics.print("You made it! Be proud of yourself!")
+	local linesTotalHeight = (fonts.text:getHeight() * #self.text)
+	local buttonOffsetY = (love.graphics.getHeight() * 0.5) - (linesTotalHeight * 0.5)
 
-	love.graphics.print("Press ESC to quit")
-	love.graphics.print("Press any key to return to the menu")
+	for index, line in pairs(self.text) do
+		love.graphics.print(line, (love.graphics.getWidth() * 0.5) - (fonts.text:getWidth(line) * 0.5), buttonOffsetY + (fonts.text:getHeight() * (index - 1)))
+	end
 end
 
 function win:keypressed(key)
