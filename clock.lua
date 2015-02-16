a51806495f6c4ab0bccd114ba021494a36d087d2
Nico Grashoff <ngrash@gmail.com>
2015-02-16 23:45:16 +0100

    Fix clock on windows

--- clock.lua
+++ clock.lua
@@ -1,7 +1,10 @@
 function secondsToClock(seconds)
-	return string.format("%.2d:%.2d", seconds/(60*60), seconds/60%60)
+	return secondsToCountdown(seconds)
+	--return os.date("!%H:%M",seconds)
+	--return string.format("%.2d:%.2d", seconds/(60*60), seconds/60%60)
 end
 
 function secondsToCountdown(seconds)
-	return string.format("%.2d:%.2d:%.2d", seconds/(60*60), seconds/60%60, seconds%60)
+	return os.date("!%X",seconds)
+	--return string.format("%.2d:%.2d:%.2d", seconds/(60*60), seconds/60%60, seconds%60)
 end
